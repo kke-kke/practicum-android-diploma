df4b49fd777e76591e7090b52334973cb0b21019
Ksenia Kovaleva <ks8niko@gmail.com>
2025-03-14 22:40:45 +0300

    в главной активити проинициализирован navController

--- app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
@@ -2,13 +2,33 @@ package ru.practicum.android.diploma.ui.root
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import ru.practicum.android.diploma.BuildConfig
 import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.ActivityRootBinding
 
 class RootActivity : AppCompatActivity() {
+    private lateinit var rootBinding: ActivityRootBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_root)
+        rootBinding = ActivityRootBinding.inflate(layoutInflater)
+        setContentView(rootBinding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.container_view) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        rootBinding.bottomNavigationView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            rootBinding.bottomNavigationView.isVisible = destination.id == R.id.searchFragment
+                || destination.id == R.id.favoritesFragment
+                || destination.id == R.id.teamFragment
+            rootBinding.navBarDivider.isVisible = destination.id == R.id.searchFragment
+                || destination.id == R.id.favoritesFragment
+                || destination.id == R.id.teamFragment
+        }
 
         // Пример использования access token для HeadHunter API
         networkRequestExample(accessToken = BuildConfig.HH_ACCESS_TOKEN)
