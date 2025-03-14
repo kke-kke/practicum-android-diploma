b6c123069f38f9a50e28c68ec758a54e15575d70
Ksenia Kovaleva <ks8niko@gmail.com>
2025-03-14 23:06:14 +0300

    изменена инициализация переменной и порядок импортов

--- app/src/main/java/ru/practicum/android/diploma/ui/JobFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/JobFragment.kt
@@ -1,19 +1,26 @@
 package ru.practicum.android.diploma.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
 import ru.practicum.android.diploma.databinding.FragmentJobBinding
 
 class JobFragment : Fragment() {
 
-    private lateinit var jobBinding: FragmentJobBinding
+    private var _binding: FragmentJobBinding? = null
+    private val jobBinding get() = _binding!!
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        jobBinding = FragmentJobBinding.inflate(inflater, container, false)
+        _binding = FragmentJobBinding.inflate(inflater, container, false)
         return jobBinding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
