680aa534b21727acf1957716b65bac47123ddc51
ElanyFly <149251217+ElanyFly@users.noreply.github.com>
2025-03-16 12:11:42 +0300

    Task4 - скобочку забыла и ненужные импорты

--- app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
@@ -2,16 +2,8 @@ package ru.practicum.android.diploma.ui.root
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import ru.practicum.android.diploma.BuildConfig
 import ru.practicum.android.diploma.R
-import ru.practicum.android.diploma.data.network.ApiService
-import ru.practicum.android.diploma.data.network.NetworkClient
-import ru.practicum.android.diploma.data.network.call
-import ru.practicum.android.diploma.domain.models.Response
-import ru.practicum.android.diploma.util.Constants
 
 class RootActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +13,6 @@ class RootActivity : AppCompatActivity() {
         // Пример использования access token для HeadHunter API
         networkRequestExample(accessToken = BuildConfig.HH_ACCESS_TOKEN)
 
-        }
     }
 
     private fun networkRequestExample(accessToken: String) {
