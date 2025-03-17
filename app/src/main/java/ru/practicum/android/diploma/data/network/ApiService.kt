50985cb589325c2f9b47e2af0e04fd66ad319ffa
elena-zavadskaya <ele.zavadskaya.na@gmail.com>
2025-03-17 20:17:48 +0300

    v1 Отредактировала порядок импортов

--- app/src/main/java/ru/practicum/android/diploma/data/network/ApiService.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/network/ApiService.kt
@@ -1,14 +1,14 @@
 package ru.practicum.android.diploma.data.network
 
-import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.Path
 import retrofit2.http.Query
 import ru.practicum.android.diploma.data.dto.models.AreasDTO
 import ru.practicum.android.diploma.data.dto.models.IndustryDTO
-import ru.practicum.android.diploma.data.dto.responses.VacanciesResponse
 import ru.practicum.android.diploma.data.dto.models.VacancyDTO
+import ru.practicum.android.diploma.data.dto.responses.VacanciesResponse
+import ru.practicum.android.diploma.domain.models.Response
 
 interface ApiService {
     @Headers(
