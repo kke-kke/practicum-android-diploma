99ebef540ae8288f6d724050d0d426003bed8b5d
Ksenia Kovaleva <ks8niko@gmail.com>
2025-03-18 22:07:35 +0300

    смена иконки в строке поиска

--- app/src/main/java/ru/practicum/android/diploma/ui/search/SearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/search/SearchFragment.kt
@@ -1,7 +1,12 @@
 package ru.practicum.android.diploma.ui.search
 
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentSearchBinding
 import ru.practicum.android.diploma.ui.BaseFragment
 
@@ -9,6 +14,41 @@ class SearchFragment : BaseFragment<FragmentSearchBinding>() {
 
     override fun onCreateBinding(inflater: LayoutInflater, container: ViewGroup?): FragmentSearchBinding {
         return FragmentSearchBinding.inflate(inflater, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.searchBar.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                if (s.isNullOrEmpty()) {
+                    setSearchIcon()
+                    hideRecycler()
+                } else {
+                    setClearIcon()
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
+
+    }
+
+    private fun setSearchIcon() {
+        binding.searchBarContainer.setEndIconDrawable(R.drawable.ic_search)
+        binding.searchBarContainer.setEndIconOnClickListener(null)
+    }
+
+    private fun setClearIcon() {
+        binding.searchBarContainer.setEndIconDrawable(R.drawable.ic_close)
+        binding.searchBarContainer.setEndIconOnClickListener {
+            binding.searchBar.text?.clear()
+        }
+    }
+
+    private fun hideRecycler() {
+        binding.searchResultRecyclerView.visibility = View.GONE
     }
 
 }
