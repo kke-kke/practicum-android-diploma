47340be7c4d5bbbd8f6c342b451b3811b85c4a8e
atimofeev <atimofeev@rnd.dkb61>
2025-03-20 13:41:30 +0300

    Реализация пагинации во ViewModel

--- app/src/main/java/ru/practicum/android/diploma/presentation/SearchViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/SearchViewModel.kt
@@ -14,41 +14,53 @@ import ru.practicum.android.diploma.util.Constants
 class SearchViewModel(private val searchVacanciesInteractor: SearchVacanciesInteractor) : ViewModel() {
 
     private var searchJob: Job? = null
-    private var currentPage: Byte = -1
-
+    private var currentPage: Int = 0
+    private var totalPages: Int = 0
+    private var lastSearchText: String = ""
 
     private val searchScreenState = MutableLiveData<SearchVacanciesResult>()
     fun observeScreenState(): LiveData<SearchVacanciesResult> = searchScreenState
 
-    private var lastSearchedText: String = ""
-
     fun searchVacancies(searchedText: String) {
-        if (searchedText.isEmpty()) {
+        if (searchedText.isEmpty() or (searchedText == lastSearchText)) {
             return
         }
 
-        if ((searchedText != lastSearchedText)) {
-            currentPage = -1
-        }
-
+        lastSearchText = searchedText
+        currentPage = 0
         searchJob?.cancel()
+
         searchJob = viewModelScope.launch {
             delay(SEARCH_DEBOUNCE_DELAY_IN_MLS)
-            viewModelScope.launch {
-                lastSearchedText = searchedText
-                val filter = mapOf(
-                    "text" to searchedText,
-                    "page" to (currentPage + 1).toString(),
-                    "per_page" to Constants.VACANCIES_PER_PAGE.toString(),
-                )
-
-                searchVacanciesInteractor.searchVacancies(filter)
-                    ?.collect { searchVacanciesResult ->
-                        searchScreenState.postValue(searchVacanciesResult)
+            searchVacanciesInteractor.searchVacancies(getFilter())
+                ?.collect { searchVacanciesResult ->
+                    searchScreenState.postValue(searchVacanciesResult)
+                    if (searchVacanciesResult is SearchVacanciesResult.Success) {
+                        totalPages = searchVacanciesResult.vacanciesFound.maxPages
                     }
-            }
+                }
         }
     }
+
+    fun getNextPartOfVacancies() {
+        if (lastSearchText.isEmpty() or (searchJob?.isActive == true) or ((currentPage+1) > totalPages)) {
+            return
+        }
+
+        searchJob = viewModelScope.launch {
+            currentPage += 1
+            searchVacanciesInteractor.searchVacancies(getFilter())
+                ?.collect { searchVacanciesResult ->
+                    searchScreenState.postValue(searchVacanciesResult)
+                }
+        }
+    }
+
+    private fun getFilter() = mapOf(
+        "text" to lastSearchText,
+        "page" to currentPage.toString(),
+        "per_page" to Constants.VACANCIES_PER_PAGE.toString(),
+    )
 
     companion object {
         private const val SEARCH_DEBOUNCE_DELAY_IN_MLS = 2000L
