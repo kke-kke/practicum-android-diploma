825c12de162735b956d34e6cfb10eb11fcff4c63
atimofeev <timtut123@gmail.com>
2025-03-23 15:51:42 +0300

    "Скелет" ui избранных вакансий

---
+++ app/src/main/java/ru/practicum/android/diploma/presentation/favourites/FavouritesViewModel.kt
@@ -0,0 +1,24 @@
+package ru.practicum.android.diploma.presentation.favourites
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import ru.practicum.android.diploma.presentation.state.VacanciesScreenState
+
+class FavouritesViewModel() : ViewModel() {
+
+    private val _uiState = MutableStateFlow<VacanciesScreenState>(VacanciesScreenState.Loading)
+    val uiState: StateFlow<VacanciesScreenState> = _uiState
+
+    init {
+        viewModelScope.launch {
+            _uiState.value = VacanciesScreenState.Loading
+            /*favouritesUseCase.getFavouriteVacancies().collect { state ->
+                //--- Обработка состояния
+            }
+        }*/
+        }
+    }
+}
--- app/src/main/java/ru/practicum/android/diploma/ui/favorites/FavoritesFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/favorites/FavoritesFragment.kt
@@ -1,14 +1,45 @@
 package ru.practicum.android.diploma.ui.favorites
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.launch
+import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.databinding.FragmentFavoritesBinding
+import ru.practicum.android.diploma.presentation.favourites.FavouritesViewModel
+import ru.practicum.android.diploma.presentation.state.VacanciesScreenState
 import ru.practicum.android.diploma.ui.BaseFragment
 
 class FavoritesFragment : BaseFragment<FragmentFavoritesBinding>() {
 
+    private val favouritesViewModel: FavouritesViewModel by viewModel()
+
     override fun onCreateBinding(inflater: LayoutInflater, container: ViewGroup?): FragmentFavoritesBinding {
         return FragmentFavoritesBinding.inflate(inflater, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.RESUMED){
+                favouritesViewModel.uiState.collect { state ->
+                    renderState(state)
+                }
+            }
+        }
+    }
+
+    private fun renderState(state: VacanciesScreenState){
+        when(state){
+            is VacanciesScreenState.Content -> {}
+            VacanciesScreenState.Empty -> {}
+            is VacanciesScreenState.Error -> {}
+            VacanciesScreenState.Loading -> {}
+        }
     }
 
 }
