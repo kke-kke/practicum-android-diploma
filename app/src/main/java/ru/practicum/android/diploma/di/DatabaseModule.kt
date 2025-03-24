bdff1443a5138cb870be67baae116134b4e2c5f4
altynchach <77624594+altynchach@users.noreply.github.com>
2025-03-25 01:19:31 +0600

    epic2_32_2

--- app/src/main/java/ru/practicum/android/diploma/di/DatabaseModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DatabaseModule.kt
@@ -1,15 +1,26 @@
 import androidx.room.Room
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 import ru.practicum.android.diploma.util.Constants
 
 val databaseModule = module {
+
+    val MIGRATION_1_2 = object : Migration(1, 2) {
+        override fun migrate(database: SupportSQLiteDatabase) {
+            database.execSQL("ALTER TABLE vacancies ADD COLUMN `alternateUrl` TEXT")
+        }
+    }
+
     single {
         Room.databaseBuilder(
             androidContext(),
             AppDatabase::class.java,
             Constants.DATABASE_NAME
-        ).build()
+        )
+            .addMigrations(MIGRATION_1_2)
+            .build()
     }
     single {
         get<AppDatabase>().vacancyDao()
