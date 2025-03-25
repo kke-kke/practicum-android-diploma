6ed73258035f1c8066261025d3d0e937f0f69a65
altynchach <77624594+altynchach@users.noreply.github.com>
2025-03-26 03:26:39 +0600

    magic numbers2

--- app/src/main/java/ru/practicum/android/diploma/data/database/Migrations.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/database/Migrations.kt
@@ -3,16 +3,17 @@ package ru.practicum.android.diploma.data.database
 import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
 
+private const val MIGRATION_VERSION_1 = 1
 private const val MIGRATION_VERSION_2 = 2
 private const val MIGRATION_VERSION_3 = 3
 
 object Migrations {
-    val MIGRATION_1_2 = object : Migration(1, 2) {
+    val MIGRATION_1_2 = object : Migration(MIGRATION_VERSION_1, MIGRATION_VERSION_2) {
         override fun migrate(database: SupportSQLiteDatabase) {
             database.execSQL("ALTER TABLE vacancies ADD COLUMN alternate_url TEXT")
         }
     }
-    val MIGRATION_2_3 = object : Migration(2, 3) {
+    val MIGRATION_2_3 = object : Migration(MIGRATION_VERSION_2, MIGRATION_VERSION_3) {
         override fun migrate(database: SupportSQLiteDatabase) {
             database.execSQL(
                 "ALTER TABLE vacancies ADD COLUMN isFavorite INTEGER NOT NULL DEFAULT 0"
