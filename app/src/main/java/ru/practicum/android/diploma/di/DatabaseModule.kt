b203ba7921f6a43b285350ee9fe8ec2c79834a0f
Ksenia Kovaleva <ks8niko@gmail.com>
2025-03-25 16:51:01 +0300

    исправлены package, добавлен addTypeConverter

--- app/src/main/java/ru/practicum/android/diploma/di/DatabaseModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DatabaseModule.kt
@@ -1,6 +1,10 @@
+package ru.practicum.android.diploma.di
+
 import androidx.room.Room
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
+import ru.practicum.android.diploma.data.database.AppDatabase
+import ru.practicum.android.diploma.data.database.Converters
 import ru.practicum.android.diploma.util.Constants
 
 val databaseModule = module {
@@ -9,7 +13,9 @@ val databaseModule = module {
             androidContext(),
             AppDatabase::class.java,
             Constants.DATABASE_NAME
-        ).build()
+        )
+            .addTypeConverter(get<Converters>())
+            .build()
     }
     single {
         get<AppDatabase>().vacancyDao()
