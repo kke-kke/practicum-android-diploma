e388f74c9bc5153463ed1fce750f021e4c7e93d3
Ksenia Kovaleva <ks8niko@gmail.com>
2025-03-25 17:27:14 +0300

    исправлен детект

--- app/src/main/java/ru/practicum/android/diploma/domain/impl/VacancyDetailsInteractorImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/impl/VacancyDetailsInteractorImpl.kt
@@ -5,12 +5,11 @@ import ru.practicum.android.diploma.domain.api.VacancyDetailsRepository
 import ru.practicum.android.diploma.domain.interactor.VacancyDetailsInteractor
 import ru.practicum.android.diploma.domain.models.VacancyDetailsStateLoad
 
-class VacancyDetailsInteractorImpl(private val vacancyDetailsRepository: VacancyDetailsRepository) : VacancyDetailsInteractor {
+class VacancyDetailsInteractorImpl(
+    private val vacancyDetailsRepository: VacancyDetailsRepository
+) : VacancyDetailsInteractor {
     override fun loadVacancyDetails(vacancyId: String): Flow<VacancyDetailsStateLoad> {
         return vacancyDetailsRepository.loadVacancy(vacancyId)
     }
 
-//    override suspend fun deleteVacancy(vacancyId: String) {
-//        vacancyDetailsRepository.deleteVacancy(vacancyId)
-//    }
 }
