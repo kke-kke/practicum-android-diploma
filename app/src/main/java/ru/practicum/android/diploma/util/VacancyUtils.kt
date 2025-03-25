daabd18a7939348a306e78317905202b95dddf1d
Ksenia Kovaleva <ks8niko@gmail.com>
2025-03-25 13:05:01 +0300

    добавлена обработка null значений

--- app/src/main/java/ru/practicum/android/diploma/util/VacancyUtils.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/VacancyUtils.kt
@@ -6,29 +6,34 @@ import java.text.DecimalFormatSymbols
 import java.util.Locale
 
 object VacancyUtils {
-    fun divideIntoDigits(number: Int): String {
+    fun divideIntoDigits(number: Int?): String {
+        if (number == null) return ""
         val russianLocale = Locale("ru", "RU")
         val russianSymbols = DecimalFormatSymbols(russianLocale)
         val russianFormatter = DecimalFormat("#,###.##", russianSymbols)
         return russianFormatter.format(number)
     }
 
-    fun getCurrencySymbol(currencyCode: String): String {
-        return Currency.entries.find { it.currency == currencyCode }?.symbol ?: currencyCode
+    fun getCurrencySymbol(currencyCode: String?): String {
+        return currencyCode?.let { code ->
+            Currency.entries.find { it.currency == code }?.symbol ?: code
+        } ?: ""
     }
 
-    fun getVacancySalary(from: Int, to: Int): String {
+    fun getVacancySalary(from: Int?, to: Int?): String {
         return when {
-            from != -1 && to != -1 -> "от ${divideIntoDigits(from)} до ${divideIntoDigits(to)}"
-            from != -1 -> "от ${divideIntoDigits(from)}"
-            to != -1 -> "до ${divideIntoDigits(to)}"
+            (from != null && from != -1) && (to != null && to != -1) -> "от ${divideIntoDigits(from)} до ${divideIntoDigits(to)}"
+            from != null && from != -1 -> "от ${divideIntoDigits(from)}"
+            to != null && to !=-1 -> "до ${divideIntoDigits(to)}"
             else -> "Зарплата не указана"
         }
     }
 
     fun Salary?.toSalaryString(): String {
         return this?.let { salary ->
-            getVacancySalary(salary.from, salary.to) + " " + getCurrencySymbol(salary.currency)
+            val salaryText = getVacancySalary(salary.from, salary.to)
+            val currencySymbol = getCurrencySymbol(salary.currency)
+            if (salaryText == "Зарплата не указана") salaryText else "$salaryText $currencySymbol"
         } ?: "Зарплата не указана"
     }
 }
