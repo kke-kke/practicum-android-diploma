d67bf206a015725dcd8af0b12c963b0bead2f865
ElanyFly <149251217+ElanyFly@users.noreply.github.com>
2025-03-26 11:44:12 +0300

    Epic1_Task26- поправлен детект

--- app/src/main/java/ru/practicum/android/diploma/presentation/search/SearchViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/search/SearchViewModel.kt
@@ -1,7 +1,6 @@
 package ru.practicum.android.diploma.presentation.search
 
 import android.app.Application
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
