ee445f773608733f392691fff06da79ea49ca7b7
altynchach <77624594+altynchach@users.noreply.github.com>
2025-03-29 19:37:15 +0600

    industry repository2

---
+++ app/src/main/java/ru/practicum/android/diploma/data/impl/AreasRepositoryImpl.kt
@@ -0,0 +1,27 @@
+package ru.practicum.android.diploma.data.impl
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import ru.practicum.android.diploma.data.mappers.toDomainList
+import ru.practicum.android.diploma.data.network.ApiService
+import ru.practicum.android.diploma.data.network.call
+import ru.practicum.android.diploma.domain.api.AreasRepository
+import ru.practicum.android.diploma.domain.models.AreaExtended
+import ru.practicum.android.diploma.domain.models.Response
+
+class AreasRepositoryImpl(
+    private val apiService: ApiService
+) : AreasRepository {
+
+    override suspend fun getAllAreas(): Result<List<AreaExtended>> {
+        return withContext(Dispatchers.IO) {
+            kotlin.runCatching {
+                val response = apiService.getAreas().call()
+                when (response) {
+                    is Response.Success -> response.data.toDomainList()
+                    is Response.Error -> throw Exception("Error loading areas: ${response.errorMessage}")
+                }
+            }
+        }
+    }
+}
