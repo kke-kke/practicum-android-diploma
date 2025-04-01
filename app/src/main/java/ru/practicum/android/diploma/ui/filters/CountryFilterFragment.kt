e5bb736662a46d5571b5830d3cc0374c47955b73
elena-zavadskaya <ele.zavadskaya.na@gmail.com>
2025-04-02 00:32:46 +0300

    epic 5 task 66 v2 - реализовала логику отображения списка стран

--- app/src/main/java/ru/practicum/android/diploma/ui/filters/CountryFilterFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/filters/CountryFilterFragment.kt
@@ -1,12 +1,69 @@
 package ru.practicum.android.diploma.ui.filters
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.databinding.FragmentCountryFilterBinding
+import ru.practicum.android.diploma.domain.models.AreaExtended
+import ru.practicum.android.diploma.presentation.filters.CountryViewModel
+import ru.practicum.android.diploma.presentation.state.CountryScreenState
 import ru.practicum.android.diploma.ui.BaseFragment
 
 class CountryFilterFragment : BaseFragment<FragmentCountryFilterBinding>() {
+
+    private val viewModel: CountryViewModel by viewModel()
+    private lateinit var countriesAdapter: CountriesAdapter
     override fun onCreateBinding(inflater: LayoutInflater, container: ViewGroup?): FragmentCountryFilterBinding {
         return FragmentCountryFilterBinding.inflate(inflater, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupRecyclerView()
+        observeViewModel()
+
+        viewModel.loadCountries()
+    }
+
+    private fun setupRecyclerView() {
+        countriesAdapter = CountriesAdapter { country ->
+        }
+        binding.countriesRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = countriesAdapter
+        }
+    }
+
+    private fun observeViewModel() {
+        viewModel.countriesState.observe(viewLifecycleOwner) { state ->
+            when (state) {
+                is CountryScreenState.Success -> {
+                    if (state.countries.isEmpty()) {
+                        showErrorView()
+                    } else {
+                        showCountriesList(state.countries)
+                    }
+                }
+                is CountryScreenState.Error -> {
+                    showErrorView()
+                }
+                else -> Unit
+            }
+        }
+    }
+
+    private fun showCountriesList(countries: List<AreaExtended>) {
+        binding.countriesRecyclerView.visibility = View.VISIBLE
+        binding.notFound.visibility = View.GONE
+        countriesAdapter.submitList(countries)
+    }
+
+    private fun showErrorView() {
+        binding.countriesRecyclerView.visibility = View.GONE
+        binding.notFound.visibility = View.VISIBLE
     }
 }
