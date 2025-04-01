26c6039accfaed79ccfc62c10b833874584fbe7b
Ksenia Kovaleva <ks8niko@gmail.com>
2025-04-02 00:40:06 +0300

    исправлен детект

--- app/src/main/java/ru/practicum/android/diploma/ui/filters/RegionFilterFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/filters/RegionFilterFragment.kt
@@ -6,7 +6,6 @@ import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 import com.google.android.material.textfield.TextInputLayout
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentRegionFilterBinding
